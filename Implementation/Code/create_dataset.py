dee0525fb530046e79a924f04941caae548a3cd6
David Angel Alba Bonilla <78463209+DavidAlba2627@users.noreply.github.com>
2023-09-17 19:32:21 -0600

    Create create_dataset.py

---
+++ Implementation/Code/create_dataset.py
@@ -0,0 +1,33 @@
+import numpy as np
+from scipy.stats import multivariate_normal
+from sklearn.preprocessing import StandardScaler
+
+# Generating a bivariate dataset with a multivariate normal distribution
+def create_bivariate(cov, mean, n_points, seed=101):
+    """
+    Parameters:
+    - cov: Covariance matrix determining the shape and orientation of the distribution.
+    - mean: Mean vector specifying the center of the distribution.
+    - n_points: Number of data points to generate.
+    - seed: Seed for the random number generator (optional).
+    Returns:
+    - X: Dataset generated from the multivariate normal distribution.
+    """
+  
+    # Defining dataset
+    distr = multivariate_normal(cov=cov, mean=mean, seed=seed)
+    X = distr.rvs(size=n_points)
+    return X
+
+# Standardize=ing the dataset to ensure that features have zero mean and unit variance.
+def standardize_data(X):
+    """
+    Parameters:
+    - X: Input dataset.
+    Returns:
+    - X_scaled: Standardized dataset.
+    """
+  
+    sc = StandardScaler()
+    X_scaled = sc.fit_transform(X)
+    return X_scaled
